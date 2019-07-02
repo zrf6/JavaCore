cf29314ef127c332469027fb5f0cefe28a530fd9
zrf6 <371118344@qq.com>
2019-07-03 03:01:41 +0800

    Hello World

---
+++ Test/src/com/java/core/test/Test.java
@@ -0,0 +1,21 @@
+/**
+ * @Title: Test.java
+ * @Description: TODO
+ * @author: bryant
+ * @date: 2019年7月3日 上午2:59:58
+ * @version: v1.0
+ */
+package com.java.core.test;
+
+/**
+ * @ClassName: Test
+ * @Description: TODO
+ * @author: bryant
+ *
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
